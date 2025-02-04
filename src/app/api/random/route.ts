0731ec65b21a90635bc30d4460acbf68d0b561c9
Tejas Thorat <tejas@netomi.com>
2025-02-04 20:33:29 +0530

    feat: update GET response to return 400 status for true data condition

--- src/app/api/random/route.ts
+++ src/app/api/random/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const randomBoolean = Math.random() >= 0.5;
   if (randomBoolean) {
-    return NextResponse.json({ data: true });
+    return NextResponse.json({ data: true }, { status: 400 });
   }
   return NextResponse.json({ data: false });
 }
