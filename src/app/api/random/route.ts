35410ab71c2fe3e8294c29d02e842f7c3fd19de5
Tejas Thorat <tejas@netomi.com>
2025-02-04 20:15:41 +0530

    feat: replace old random API with new route-based implementation for improved response handling

---
+++ src/app/api/random/route.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const randomBoolean = Math.random() >= 0.5;
+  if (randomBoolean) {
+    return NextResponse.json({ data: true });
+  }
+  return NextResponse.json({ data: false });
+}
